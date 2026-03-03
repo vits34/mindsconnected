0b866a9b7de4e34702d880dc9c17a1b129248ae0
Vitor Henriques <vits34@users.noreply.github.com>
2026-03-03 10:44:27 +0000

    Add deploy script

---
+++ deploy.bat
@@ -0,0 +1,20 @@
+@echo off
+echo.
+echo  Deploying Minds Connected website...
+echo.
+
+cd /d "%~dp0"
+
+git add -A
+
+set /p MSG="Commit message (or press Enter for 'Update website'): "
+if "%MSG%"=="" set MSG=Update website
+
+git commit -m "%MSG%"
+
+git push origin master
+
+echo.
+echo  Done! Your changes are live at https://vits34.github.io/mindsconnected/
+echo.
+pause
